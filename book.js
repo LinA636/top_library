cd16dd608f2f3e55d3f740654731097f75a3d3ad
LinA <lina.franssen@gmail.com>
2024-07-01 14:48:55 +0200

    add delete button to book-card

--- book.js
+++ book.js
@@ -1,3 +1,5 @@
+const myLibrary = [];
+
 function Book(title, author, pageNumber) {
   this.title = title;
   this.author = author;
@@ -18,45 +20,89 @@ function Book(title, author, pageNumber) {
 }
 
 function addBookToLibrary(book) {
-  myLibrary << book;
-}
-
-function displayLibrary(){
-  myLibrary.map((book) => displayBook(book));
+  myLibrary.push(book);
+  displayBook(book);
 }
 
 function displayBook(book) {
-  const newTitle = book.title;
-  const isRead = read ? " This book is read." : "This book is not yet read";
-  const newDescription = "by " + book.author + " has " + book.pageNumber + " pages." + isRead;
+  /* create a book card */
+  const bookCard = document.createElement('div');
+  bookCard.classList.add('book-card');
 
+  /** create content of book-card */
+  const bookTitle = document.createElement('h2');
+  bookTitle.textContent = book.title;
   
-  displayElement = document.getElementById("my-library");
-  const newDiv = document.createElement("div");
-  const h2 = document.createElement("h2");
-  const p = document.createElement("p");
+  const bookDescription = document.createElement('p');
+  bookDescription.textContent = "by " + book.author + " has " + book.pageNumber + " pages.";
 
-  h2.appendChild(newTitle);
-  p.appendChild(newDescription);
-  newDiv.appendChild(h2);
-  newDiv.appendChild(p);
-  displayElement.appendChild(newDiv);
+  const deleteButton = document.createElement('button');
+  deleteButton.textContent = "delete book";
+  deleteButton.addEventListener('click', () => {deleteBook(book, bookCard);});
+
+
+  /** append the content to the book-card */
+  bookCard.appendChild(bookTitle);
+  bookCard.appendChild(bookDescription);
+  bookCard.appendChild(deleteButton);
+
+  /** append book-card to view */
+  const libraryContainer = document.getElementById('book-cards-container');
+  libraryContainer.appendChild(bookCard);
+
 }
+
+function deleteBook(book, bookCard){
+  /** find index of the book in the myLibrary index */
+  const index = myLibrary.indexOf(book);
+
+  /** remove book from the library */
+  if (index > -1){
+    myLibrary.splice(index,1);
+  }
+
+  /** remove book-card from the DOM */
+  bookCard.remove();
+}
+
+function showForm(){
+  document.getElementById('add-book-form').style.display = 'block';
+}
+
+function handleFormSubmit(event){
+  event.preventDefault();
+
+  /**Get form values */
+  const bookTitle = document.getElementById('title').value;
+  const bookAuthor = document.getElementById('author').value;
+  const bookPages = document.getElementById('pageNumber').value;
+
+  /** Create book object */
+  const newBook = new Book(bookTitle, bookAuthor, bookPages);
+
+  addBookToLibrary(newBook);
+
+  /** Clear and hide the form */
+  document.getElementById('add-book-form').reset();
+  document.getElementById('add-book-form').style.display = 'none'; 
+}
+
+/** Event listener to handle ad book form */
+document.getElementById('open-book-form-button').addEventListener('click', showForm);
+document.getElementById('add-book-form').addEventListener('submit', handleFormSubmit);
+
+
 
 
 
 /* Code to execute */
-const theHobbit = new Book('The Hobbit', 'J.R.R. Tolkien', 295);
-const eragon = new Book("Eragon", "Christopher Paolini", 1000);
-const book2 = new Book("book title", 'auhtor', 500);
-const book3 = new Book('book 2', 'author3', 4009);
-
-
-const myLibrary = [];
-
-addBookToLibrary(theHobbit);
-addBookToLibrary(eragon);
-addBookToLibrary(book2);
-addBookToLibrary(book3);
-
-displayLibrary();
+  const theHobbit = new Book('The Hobbit', 'J.R.R. Tolkien', 295);
+  const eragon = new Book("Eragon", "Christopher Paolini", 1000);
+  const book2 = new Book("book title", 'auhtor', 500);
+  const book3 = new Book('book 2', 'author3', 4009);
+  
+  displayBook(theHobbit);
+  addBookToLibrary(theHobbit);
+  addBookToLibrary(eragon);
+  addBookToLibrary(book2);
+  addBookToLibrary(book3);
